6a8a214285bce03ab7b23f3574f94e5bbab52e86
Spencer Eldred <eldredspencer@gmail.com>
2013-08-24 05:02:53 -1000

    Add min password validation and rspec test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -34,7 +34,7 @@ describe User do
 	describe "when email format is invalid" do
 		it "should be invalid" do
 			addresses = %w[user@foo,com user_at_foo.org example.user@foo. 
-				foo@bar_baz.com foo@bar+baz.com]
+				foo@bar_baz.com foo@bar+baz.com foo@bar..com]
 			addresses.each do |invalid_address|
 				@user.email = invalid_address
 				expect(@user).not_to be_valid
@@ -102,6 +102,16 @@ describe User do
 
 			it { should_not eq user_for_invalid_password }
 			specify { expect(user_for_invalid_password).to be_false }
+		end
+	end
+
+	describe "email with mixed case" do
+		let(:mixed_case_email) { "fooBAR@exAmPLe.COm" }
+
+		it "should be saved as lower case" do
+			@user.email = mixed_case_email
+			@user.save
+			expect(@user.reload.email).to eq mixed_case_email.downcase
 		end
 	end
 
