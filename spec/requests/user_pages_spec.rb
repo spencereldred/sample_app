678ab4ccf22f0d46e17c6b3900e525588f705726
Spencer Eldred <eldredspencer@gmail.com>
2013-08-26 09:16:52 -1000

    Update test of user error messages - update flash - ch7 exercises

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -37,8 +37,37 @@ describe "UserPages" do
 				fill_in "Confirmation", 	with: "foobar"
 			end
 
+			describe "after saving the user" do
+				before { click_button submit }
+				let(:user) { User.find_by(email: 'user@example.com') }
+
+				it { should have_title(user.name) }
+				it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+			end
+
 			it "should create a user" do
 				expect { click_button submit }.to change(User, :count).by(1)
+			end
+		end
+
+		describe "with invalid information" do
+			before do
+				fill_in "Name",				with: ""
+				fill_in "Email", 			with: ""
+				fill_in "Password",			with: ""
+				fill_in "Confirmation", 	with: ""
+			end
+
+			describe "after submission" do
+				before { click_button submit }
+
+				it { should have_title('Sign up') }
+				it { should have_content('error') }
+				it { should have_content("Name can't be blank") }
+				it { should have_content("Email can't be blank") }
+				it { should have_content("Email is invalid") }
+				it { should have_content("Password can't be blank") }
+				it { should have_content("Password is too short") }
 			end
 		end
 	end
